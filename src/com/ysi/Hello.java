5e7c85f6b2c5e622ed90bbdb6e082a8a5cb7adf9
ysi <kyoyaiida@naver.com>
2018-05-26 15:20:44 +0900

    develop add2

--- src/com/ysi/Hello.java
+++ src/com/ysi/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");
 		System.out.println("develop add1!");
+		System.out.println("develop add12");
 	}
 }
